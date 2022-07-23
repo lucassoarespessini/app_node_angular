49fd1e06d47a6d7c656e7bc5fdcf94dd3cb5f6ad
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2022-07-23 15:59:53 -0300

    Its works list

--- frontend/src/app/components/produto-listagem/produto-listagem.component.ts
+++ frontend/src/app/components/produto-listagem/produto-listagem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from './../../service/crud.service';
 
 @Component({
   selector: 'app-produto-listagem',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoListagemComponent implements OnInit {
 
-  constructor() { }
-
+  Produtos:any = [];
+  constructor(private crudService: CrudService) { }
   ngOnInit(): void {
+    this.crudService.GetProdutos().subscribe(res => {
+      console.log(res)
+      this.Produtos =res;
+    });    
+  }
+  delete(id:any, i:any) {
+    console.log(id);
+    if(window.confirm('Do you want to go ahead?')) {
+      this.crudService.deleteProduto(id).subscribe((res) => {
+        this.Produtos.splice(i, 1);
+      })
+    }
   }
 
 }
