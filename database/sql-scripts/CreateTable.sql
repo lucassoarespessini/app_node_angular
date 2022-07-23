05fc9593e11ad4e9dacfda4a93c93cb1c108dc03
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2022-07-23 14:26:27 -0300

    Add table produtos

--- database/sql-scripts/CreateTable.sql
+++ database/sql-scripts/CreateTable.sql
@@ -1,9 +1,14 @@
--- Table structure for users
-  CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS produtos;
+
+  CREATE TABLE IF NOT EXISTS produtos (
     id int(11) NOT NULL,
-    name varchar(200) NOT NULL,
-    email varchar(200) NOT NULL,
+    nome varchar(150) NOT NULL,
+    imagem varchar(200) NOT NULL,
+    descricao varchar(2000) NOT NULL,
+    estoque varchar(2000) NOT NULL,
+    status boolean NOT NULL,
+    preco float NOT NULL,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-  ALTER TABLE users ADD PRIMARY KEY (id);
-  ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+  ALTER TABLE produtos ADD PRIMARY KEY (id);
+  ALTER TABLE produtos MODIFY id int(11) NOT NULL AUTO_INCREMENT;
