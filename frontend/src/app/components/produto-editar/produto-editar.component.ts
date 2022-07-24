ad9adfd21e2facd7ae617d3f92c0ea681fe4c721
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2022-07-24 10:13:19 -0300

    Its works

--- frontend/src/app/components/produto-editar/produto-editar.component.ts
+++ frontend/src/app/components/produto-editar/produto-editar.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, NgZone } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CrudService } from './../../service/crud.service';
 import { FormGroup, FormBuilder } from "@angular/forms";
+import { HttpClientModule, HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
 
 @Component({
   selector: 'app-produto-editar',
@@ -11,13 +16,16 @@ import { FormGroup, FormBuilder } from "@angular/forms";
 export class ProdutoEditarComponent implements OnInit {
   getId: any;
   updateForm: FormGroup;
-  
+  fileName: any = [];
+  REST_API: string = 'http://' + environment.LOCAL_HOST + ':' + environment.LOCAL_PORT;
+
   constructor(
     public formBuilder: FormBuilder,
     private router: Router,
     private ngZone: NgZone,
     private activatedRoute: ActivatedRoute,
-    private crudService: CrudService
+    private crudService: CrudService,
+    private http: HttpClient
   ) {
     this.getId = this.activatedRoute.snapshot.paramMap.get('id');
     this.crudService.GetProduto(this.getId).subscribe(res => {
@@ -29,6 +37,7 @@ export class ProdutoEditarComponent implements OnInit {
         status: res['data']['status'],
         preco: res['data']['preco']
       });
+      this.fileName = res['data']['imagem'];
     });
     this.updateForm = this.formBuilder.group({
       nome: [''],
@@ -40,14 +49,52 @@ export class ProdutoEditarComponent implements OnInit {
     })
   }
   ngOnInit() { }
+  onFileSelected(event: any) {
+
+    const file: File = event.target.files[0];
+
+    if (file) {
+
+      console.log(file)
+
+      const formData = new FormData();
+
+      formData.append("file", file);
+      formData.append("file_name", this.fileName);
+
+      const upload$ = this.http.post(`${this.REST_API}/upload`, formData).pipe(map((res: any) => {
+        return res || {}
+      }),
+        catchError(this.handleError)
+      );
+
+      upload$.subscribe(res => {
+        this.fileName = res['data'];
+      });
+
+    }
+  }
   onUpdate(): any {
+    this.updateForm.value.imagem = this.fileName;
     this.crudService.updateProduto(this.getId, this.updateForm.value)
-    .subscribe(() => {
+      .subscribe(() => {
         console.log('Produto updated successfully!')
         this.ngZone.run(() => this.router.navigateByUrl('/produto-listagem'))
       }, (err) => {
         console.log(err);
-    });
+      });
+  }
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Handle client error
+      errorMessage = error.error.message;
+    } else {
+      // Handle server error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
   }
 
 }
