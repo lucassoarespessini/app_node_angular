a23e83ee64dd2b77d4256e6f3a4534b158df834e
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2022-07-24 20:29:32 -0300

    Take out insert

--- database/sql-scripts/InsertInto.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (id, name, email, created_at) VALUES
-  (1, 'Test', 'test@g.co', '2019-02-28 13:20:20'),
-  (2, 'john', 'john@g.co', '2019-02-28 13:20:20'),
-  (3, 'tus', 'tuts@g.co', '2019-02-28 13:20:20'),
-  (4, 'tut', 'tut@g.co', '2019-02-28 13:20:20'),
-  (5, 'mhd', 'mhd@g.co', '2019-02-28 13:20:20');
