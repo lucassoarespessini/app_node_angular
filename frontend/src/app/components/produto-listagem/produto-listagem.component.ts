4369ed62f0868d70da4693bb82deaadf62162c02
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2022-07-24 00:20:19 -0300

    Add images on view

--- frontend/src/app/components/produto-listagem/produto-listagem.component.ts
+++ frontend/src/app/components/produto-listagem/produto-listagem.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from './../../service/crud.service';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
   selector: 'app-produto-listagem',
@@ -8,17 +10,18 @@ import { CrudService } from './../../service/crud.service';
 })
 export class ProdutoListagemComponent implements OnInit {
 
-  Produtos:any = [];
+  Produtos: any = [];
+  REST_API: string = 'http://' + environment.LOCAL_HOST + ':' + environment.LOCAL_PORT;
   constructor(private crudService: CrudService) { }
   ngOnInit(): void {
     this.crudService.GetProdutos().subscribe(res => {
       console.log(res)
-      this.Produtos =res;
-    });    
+      this.Produtos = res;
+    });
   }
-  delete(id:any, i:any) {
+  delete(id: any, i: any) {
     console.log(id);
-    if(window.confirm('Do you want to go ahead?')) {
+    if (window.confirm('Do you want to go ahead?')) {
       this.crudService.deleteProduto(id).subscribe((res) => {
         this.Produtos.splice(i, 1);
       })
