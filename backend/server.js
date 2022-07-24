df8ec60736e1012d0793522607c8c4aee2f16e38
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2022-07-24 16:11:48 -0300

    Add cadastro produto

--- backend/server.js
+++ backend/server.js
@@ -36,72 +36,7 @@ var dbConn = mysql.createConnection({
 
 // connect to database
 dbConn.connect();
-// Retrieve all users 
-app.get('/users', function (req, res) {
-    dbConn.query('SELECT * FROM users', function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'users list.' });
-    });
-});
-// Retrieve user with id 
-app.get('/read-user/:id', function (req, res) {
 
-    let user_id = req.params.id;
-
-    if (!user_id) {
-        return res.status(400).send({ error: true, message: 'Please provide user_id' });
-    }
-
-    dbConn.query('SELECT * FROM users where id=?', user_id, function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results[0], message: 'users list.' });
-    });
-
-});
-// Add a new user  
-app.post('/add-user', function (req, res) {
-
-    let name = req.body.name;
-    let email = req.body.email;
-
-    if (!name) {
-        return res.status(400).send({ error: true, message: 'Please provide user' });
-    }
-
-    dbConn.query("INSERT INTO users (name,email) VALUES (?,?) ", [name, email], function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
-    });
-});
-//  Update user with id
-app.put('/update-user/:id', function (req, res) {
-
-    let user_id = req.params.id;
-    let name = req.body.name;
-    let email = req.body.email;
-
-    if (!user_id || !name || !email) {
-        return res.status(400).send({ error: user, message: 'Please provide user and user_id' });
-    }
-
-    dbConn.query("UPDATE users SET name = ?, email = ? WHERE id = ?", [name, email, user_id], function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'user has been updated successfully.' });
-    });
-});
-//  Delete user
-app.delete('/delete-user/:_id', function (req, res) {
-    let user_id = req.params._id;
-
-
-    if (!user_id) {
-        return res.status(400).send({ error: true, message: 'Please provide user_id' });
-    }
-    dbConn.query('DELETE FROM users WHERE id = ?', [user_id], function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results, message: 'User has been updated successfully.' });
-    });
-});
 app.post('/upload', function (req, res) {
 
     let file = req['files'].file;
--- database/sql-scripts/CreateTable 2 .sql
+++
@@ -1,9 +0,0 @@
--- Table structure for users
-  CREATE TABLE IF NOT EXISTS users (
-    id int(11) NOT NULL,
-    name varchar(200) NOT NULL,
-    email varchar(200) NOT NULL,
-    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-  ALTER TABLE users ADD PRIMARY KEY (id);
-  ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT;
